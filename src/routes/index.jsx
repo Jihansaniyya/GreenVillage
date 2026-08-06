0a7d37ece86a51774ccaf36f9eacf58730b49dbd
unknown <jihansaniyyaa@students.undip.ac.id>
2026-08-06 23:50:13 +0700

    benerin tulisan

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,7 +7,6 @@ const Home = lazy(() => import('../pages/Home'));
 const About = lazy(() => import('../pages/About'));
 const Plants = lazy(() => import('../pages/Plants'));
 const PlantDetail = lazy(() => import('../pages/PlantDetail'));
-const Contact = lazy(() => import('../pages/Contact'));
 
 const router = createBrowserRouter([
   {
@@ -18,7 +17,6 @@ const router = createBrowserRouter([
       { path: 'about', element: <About /> },
       { path: 'plants', element: <Plants /> },
       { path: 'plants/:slug', element: <PlantDetail /> },
-      { path: 'contact', element: <Contact /> },
     ],
   },
 ]);
